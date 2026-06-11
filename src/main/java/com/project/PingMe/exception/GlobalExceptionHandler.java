833fad42afbf23bc5c2953a35b5dd383304add8a
Sankalp Tiwari <sankalptiwari94255@gmail.com>
2026-06-11 11:52:48 +0530

    Created GlobalExceptionHandler Class to handle Exceptions More Efficiently

--- src/main/java/com/project/PingMe/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/PingMe/exception/GlobalExceptionHandler.java
@@ -1,4 +1,20 @@
 package com.project.PingMe.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RateLimitException.class)
+    public ResponseEntity<String> handleRateLimitException(RateLimitException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.TOO_MANY_REQUESTS);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
